febfff676001e9f8f393ff4c33eb999f9cb09c31
abir4u <abirpal90@gmail.com>
2020-03-29 13:22:45 +1300

    My first demo commit

---
+++ src/Components/Contacts/Contacts.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../Stylesheets/Contacts/Contacts.css";
+
+export default class Contacts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        This is Contacts
+        <input id="blabla" />
+        <input id="blabla" />
+        <input id="blabla" />
+      </div>
+    );
+  }
+}
---
+++ src/Components/DBC/DBC.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class DBC extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        This is DBC
+        <input id="blabla" />
+        <input id="blabla" />
+        <input id="blabla" />
+      </div>
+    );
+  }
+}
