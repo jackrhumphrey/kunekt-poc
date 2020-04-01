11b3f38fd78b78e37c7ecdcb530fdb4357e3eda0
Holly-Horwood <holly.horwood@live.com>
2020-04-01 14:58:06 +1300

    added QR image to DBC component

--- src/Components/DBC/DBC.js
+++ src/Components/DBC/DBC.js
@@ -1,4 +1,6 @@
 import React from "react";
+import qr from '../../qr.png'
+
 
 export default class DBC extends React.Component {
   constructor(props) {
@@ -8,6 +10,8 @@ export default class DBC extends React.Component {
   render() {
     return (
       <div>
+        <img src={qr} alt="QR Code" style={{ width: '200px', height: '200px' }}/>
+        <hr></hr>
         This is DBC
         <input id="blabla" />
         <input id="blabla" />
