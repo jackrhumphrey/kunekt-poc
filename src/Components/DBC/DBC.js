63d3fe07728f2c7d764b314369b401a20b0c17c6
Holly-Horwood <holly.horwood@live.com>
2020-04-16 11:37:09 +1200

    Added container flexbox styling to DBC component

--- src/Components/DBC/DBC.js
+++ src/Components/DBC/DBC.js
@@ -1,6 +1,12 @@
 import React from "react";
 import qr from "../../Images/qr.jpg";
-import ph from "../../Images/ph-img.png"
+import wa from "../../Images/whatsapp.png"
+import gm from "../../Images/gmail.png"
+import ig from "../../Images/instagram.png"
+import tw from "../../Images/twitter.png"
+import wb from "../../Images/web.png"
+import yt from "../../Images/youtube.png"
+import li from "../../Images/linkedin.png"
 
 
 export default class DBC extends React.Component {
@@ -12,18 +18,41 @@ export default class DBC extends React.Component {
     return (
       <div className="container">
         <div className="qr-img">
-          <img src={qr} alt="QR Code" style={{ width: '200px', height: '200px' }}/>
+          <img src={qr} alt="QR Code" style={{ width: '150px', height: '150px' }}/>
           <hr></hr>
         </div>
-        <div>
-          <h3 className='title'>Private</h3>
+        <div className="titles">
+          <h3 className='title' >Private</h3>
           <h3 className='title'>Work</h3>
           <h3 className='title'>Custom</h3>
-        </div> 
+        </div>  
         <div className="contact-opt">
-          <img src={ph} alt="placeholder"/>
-          <p>Phone</p>
-          
+          <img className="contact-icon" src={wa} alt="whatsapp"/>
+          <p className="contact-descption">WhatsApp</p>
+        </div>
+        <div className="contact-opt">
+          <img className="contact-icon" src={gm} alt="gmail"/>
+          <p className="contact-descption">Gmail</p>
+        </div>
+        <div className="contact-opt">
+          <img className="contact-icon" src={ig} alt="instagram"/>
+          <p className="contact-descption">Instagram</p>
+        </div>
+        <div className="contact-opt">
+          <img className="contact-icon" src={tw} alt="twitter"/>
+          <p className="contact-descption">Twitter</p>
+        </div>
+        <div className="contact-opt">
+          <img className="contact-icon" src={wb} alt="twitter"/>
+          <p className="contact-descption">Website</p>
+        </div>
+        <div className="contact-opt">
+          <img className="contact-icon" src={yt} alt="youtube"/>
+          <p className="contact-descption">YouTube</p>
+        </div>
+        <div className="contact-opt">
+          <img className="contact-icon" src={li} alt="linkedin"/>
+          <p className="contact-descption">LinkedIn</p>
         </div>
       </div>
         
